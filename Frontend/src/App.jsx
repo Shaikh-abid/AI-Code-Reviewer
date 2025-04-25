d2df2628a304bed161ac0550f205d67dd3871055
Shaikh-abid <141109084+Shaikh-abid@users.noreply.github.com>
2025-04-25 21:48:32 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
+    const response = await axios.post("https://ai-code-reviewer-backend-x5uh.onrender.com", {
       code,
     });
     setReview(response.data);
